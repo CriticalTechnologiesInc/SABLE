8dbf7356145fd729e94e1156f829124cf56aae1c
Scott Constable <sdconsta@syr.edu>
2017-02-20 22:30:26 -0500

    refactored hmac.* to use exceptions

--- include/hmac.h
+++ include/hmac.h
@@ -5,6 +5,9 @@ typedef struct {
   SHA1_Context sctx;
 } HMAC_Context;
 
-void hmac_init(HMAC_Context *ctx, const BYTE *key, UINT32 keySize);
-void hmac(HMAC_Context *ctx, const void *data, UINT32 dataSize);
-void hmac_finish(HMAC_Context *ctx);
+/* EXCEPT: ERROR_SHA1_DATA_SIZE */
+RESULT hmac_init(HMAC_Context *ctx, const BYTE *key, UINT32 keySize);
+/* EXCEPT: ERROR_SHA1_DATA_SIZE */
+RESULT hmac(HMAC_Context *ctx, const void *data, UINT32 dataSize);
+/* EXCEPT: ERROR_SHA1_DATA_SIZE */
+RESULT hmac_finish(HMAC_Context *ctx);
