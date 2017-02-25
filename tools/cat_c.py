afabaf28510f53cb772b45c39445c3c35612011d
Scott Constable <sdconsta@syr.edu>
2017-02-24 20:56:44 -0500

    added python scripts to assist with minimal header file generation

---
+++ tools/cat_c.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+def main():
+    for fp in sys.argv[1:]:
+        f = open(fp, 'r')
+        print('#line 1 \"', fp, '\"')
+        print(f.read())
+        f.close()
+
+if __name__ == '__main__':
+    main()
---
+++ tools/cat_macros.py
@@ -0,0 +1,17 @@
+import sys
+
+def main():
+    for fp in sys.argv[1:]:
+        f = open(fp, 'r')
+        print_next = False
+        for l in f.readlines():
+            if l[0] == '#' or print_next:
+                print(l[:-1])
+            if len(l) > 1 and l[-2] == '\\':
+                print_next = True
+            else:
+                print_next = False
+        f.close()
+
+if __name__ == '__main__':
+    main()
