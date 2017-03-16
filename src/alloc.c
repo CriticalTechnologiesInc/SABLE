04a5407825f760d37f9171cb19de0a0190995aca
Scott Constable <sdconsta@syr.edu>
2017-03-16 14:41:14 -0700

    more changes to heap

--- src/alloc.c
+++ src/alloc.c
@@ -45,26 +45,31 @@ struct mem_node {
 };
 static struct mem_node *node = NULL;
 
-#define BLOCK_SIZE sizeof(struct mem_node)
+#define BLOCK_SIZE 4 // must be at least sizeof(struct mem_node)
+#define BITS_ALIGN 2 // must be log_2 of BLOCK_SIZE
+#if (!(1 << BITS_ALIGN == BLOCK_SIZE))
+#error "BITS_ALIGN is not log_2 of BLOCK_SIZE
+#endif
+
 static struct mem_node heap[8 * KB / BLOCK_SIZE];
 
 void init_heap(void) {
   ASSERT(!node);
   node = heap;
   *node =
-      (struct mem_node){.size = (sizeof(heap) / BLOCK_SIZE) - 1, .next = 0};
+      (struct mem_node){.size = (sizeof(heap) >> BITS_ALIGN) - 1, .next = 0};
 }
 
 void *alloc(UINT16 size) {
   ASSERT(size > 0);
-  if (node->size < size)
+  UINT16 blocks = (size >> BITS_ALIGN) + 1; // FIXME: we might be overallocating here!
+  if (node->size <= blocks)
     return NULL;
 
-  UINT16 blocks = (size + BLOCK_SIZE - 1) / BLOCK_SIZE;
-  UINT16 offset = (node - heap) + blocks + 1;
-  struct mem_node *new_node = (struct mem_node *)(heap + offset);
-  *new_node = (struct mem_node){.size = node->size - (blocks + 1), .next = 0};
-  *node = (struct mem_node){.size = blocks, .next = offset};
+  struct mem_node *new_node = (struct mem_node *)(node + (blocks + 1));
+  UINT16 prev_size = node->size;
+  *node = (struct mem_node){.size = blocks, .next = new_node - heap};
+  *new_node = (struct mem_node){.size = prev_size - (blocks + 1), .next = 0};
   void *ret = (void *)(node + 1);
   node = new_node;
   return ret;
