74b650df06cbaaaa1edec89b6ca6990a774526f8
Scott Constable <sdconsta@syr.edu>
2017-04-12 10:23:56 -0400

    cleaned up some stuff in cleanup/asm.S

--- src/cleanup/cleanup.c
+++ src/cleanup/cleanup.c
@@ -1,17 +1,3 @@
-/*
- * \brief   Beirut - hashes command lines
- * \date    2006-06-07
- * \author  Bernhard Kauer <kauer@tudos.org>
- */
-/*
- * Copyright (C) 2006,2007,2010  Bernhard Kauer <kauer@tudos.org>
- * Technische Universitaet Dresden, Operating Systems Research Group
- *
- * This file is part of the OSLO package, which is distributed under
- * the  terms  of the  GNU General Public Licence 2.  Please see the
- * COPYING file for details.
- */
-
 #include "mbi.h"
 #include "elf.h"
 #include "util.h"
@@ -19,9 +5,11 @@
 int
 __main(struct mbi *mbi, unsigned flags)
 {
+#ifndef NDEBUG
   out_string("Zeroing out SLB memory\n");
+  wait(2000);
+#endif
   memset((void *) 0x100000, 0, 0x10000);
-  wait(5000);
 
   RESULT res = start_module(mbi);
   CATCH_ANY(res.exception, {
