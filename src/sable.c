e6144a1977d75be89354ac2e5ebfac31dfca4f84
Bhushan Jagtap <bmjagtap@syr.edu>
2017-08-11 11:55:44 -0400

    Fixing loader script for Intel

--- src/sable.c
+++ src/sable.c
@@ -419,7 +419,7 @@ static RESULT prepare_tpm(void) {
 RESULT pre_launch(struct mbi *m, unsigned flags) {
   RESULT ret = {.exception.error = NONE};
   out_string(version_string);
-  out_string("I am in pre_launch 012\n");
+  out_string("I am in pre_launch 013\n");
   out_description("Bhushan: module count", m->mods_count);
 
 
