51c5e0ac9c214d4f16223df55a75adbbb070ac3e
Scott Constable <sdconsta@syr.edu>
2018-01-10 09:10:38 -0800

    removed some debugging printouts

--- src/elf.c
+++ src/elf.c
@@ -127,8 +127,6 @@ RESULT start_module(struct mbi *mbi) {
     elf = (struct eh *)m->mod_start;
     elf_magic = (unsigned int *)elf->e_ident;
     elf_class_data = (unsigned short *)(elf->e_ident + 4);
-    out_description("elf magic:", *elf_magic);
-    out_description("elf class_data:", *elf_class_data);
 
     ERROR(*elf_magic != 0x464c457f || *elf_class_data != 0x0101,
           ERROR_BAD_ELF_HEADER, "ELF header incorrect");
